15b816ae6630b47c84ee0676fe18f2afc6e93e14
abaym <abaym.kz@gmail.com>
2022-08-21 15:23:58 +0600

    End of Section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -55,10 +55,11 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
     
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(map(response => {
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+      .pipe(map(response => {
       this.memberCache.set(Object.values(userParams).join('-'), response);
       return response;
-    }));
+    }))
   }
 
   getMember(username: string) {
@@ -86,6 +87,16 @@ export class MembersService {
   deletePhoto(photoId: number) 
   {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
   
   private getPaginatedResult<T>(url, params) {
